139959979e20ad613479d61e9caddca33a8401cc
JoeYFH <44156001+JoeYFH@users.noreply.github.com>
2020-06-08 18:12:56 +0800

    2020/6/8
    第一天試寫

---
+++ test2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test2
+{
+    class Progam
+    {
+
+
+        class Member
+        {
+            private string name = "黃義豐";
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+
+        static void Main()
+        {
+            Member a = new Member();
+            Console.WriteLine("新進員工名稱:{0}", a.Name);
+        }
+
+        
+    }
+}
